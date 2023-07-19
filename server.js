a6d8c72ea7866cc4ac10d456eb9bf98cfc4cf3d5
Fatima Mahsud <fatimamahsud1@gmail.com>
2023-07-19 12:30:50 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+
+app.post('/', function(req, res) {
+    var number1 = parseInt(req.body.num1);
+    var number2 = parseInt(req.body.num2);
+    var result = number1 - number2;
+    console.log(number1 - number2)
+    res.send("<h1>The result of the addition is: </h1>" + result)
+  })
+
+app.listen(3000)
